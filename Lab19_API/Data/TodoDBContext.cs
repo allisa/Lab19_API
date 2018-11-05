ef866867af8ea766e5417ffb143d6e1b59cb31c7
Allisa LeBeuf <al5906@gmail.com>
2018-11-04 20:11:16 -0800

    added to todo controller

---
+++ Lab19_API/Data/TodoDBContext.cs
@@ -0,0 +1,26 @@
+﻿using Lab19_API.Controllers;
+using Lab19_API.Models;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Lab19_API.Data
+{
+    public class TodoDBContext : DbContext
+    {
+        public TodoDBContext(DbContextOptions<TodoDBContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<Todo> TodoItems { get; set; }
+        public DbSet<TodoList> TodoLists { get; set; }
+
+        public static implicit operator TodoDBContext(TodoController v)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ Lab19_API/Models/Todo.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Lab19_API.Models
+{
+    public class Todo
+    {
+        public int ID { get; set; }
+        public string Name { get; set; }
+        public bool IsComplete { get; set; }
+    }
+}
